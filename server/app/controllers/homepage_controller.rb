55d1c4b96427cebea4a8bb9a241828a092ac1574
znicholson92 <znichol2@illinois.edu>
2013-03-26 13:38:23 -0500

    Homepage controller now builds input.json from database

--- server/app/controllers/homepage_controller.rb
+++ server/app/controllers/homepage_controller.rb
@@ -19,12 +19,12 @@ class HomepageController < ApplicationController
 	def processInput(input,issue)
 		
 		part = Participant.first_or_create({:user_name =>issue["author"]},{:link=>issue["authorLink"]})
-		iss = Issue.first_or_create({:link => issue["link"]},{:title => issue["title"],:status =>issue["status"],:participant=>part})
+		iss = Issue.first_or_create({:title => issue["title"]},{:status =>issue["status"],:participant=>part,:link => issue["link"]})
 		
 			
 		input.each do |curr|	
 			part = Participant.first_or_create({:user_name =>curr["author"]},{:link=>curr["authorLink"]})
-			comment = Comment.first_or_create(:title => curr["title"])
+			comment = Comment.first_or_create(:link => curr["link"])
 			comment.attributes = {
 						:title => curr["title"],
 						:link => curr["link"],
@@ -45,14 +45,41 @@ class HomepageController < ApplicationController
 			
 			comment.raise_on_save_failure = true
 			comment.save
-		end
-		
+		end	
+	end
+	
+	def find_conversations
+		#comments=
 	end
 
-	def input
-		#render :json => @data, :callback =>params[:callback]
-		send_file(@@data,
-  			:filename => "input",
-  			:type => "application/json")
+	def input	
+		comments_json=Array.new
+		issue_title = "Add colouring (and description) to password checker"
+		issue = Issue.first(:title => issue_title)
+		comments=Comment.all(:issue => issue)
+		count=0
+		comments.each do |curr|
+			curr_json=Hash.new
+			curr_json["title"]=comments[count].title
+			curr_json["link"]=comments[count].link
+			curr_json["author"]=comments[count].participant.user_name
+			curr_json["authorLink"]=comments[count].participant.link
+			curr_json["content"]=comments[count].content
+			curr_json["tags"]=Array.new
+			curr_json["status"]="Ongoing"
+			curr_json["comments"]=Array.new
+			curr_json["idea"]="#1"
+			curr_json["tone"]="positive"
+			curr_json["criteria"]=Array.new
+			comments_json[count]=curr_json
+			count=count+1
+		end
+		final_json=Hash.new
+		final_json["issueComments"]=comments_json	
+		tmp_file = "#{Rails.root}/out.txt"
+		File.open(tmp_file, 'wb') do |f|
+			f.write issue
+		end
+		render :json => final_json.to_json
 	end
 end
